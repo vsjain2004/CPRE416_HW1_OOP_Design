53f7366b2acbbe78aafd159841adbb5e9a9a6fe9
Autrin <autrinhakimi@gmail.com>
2025-03-07 15:50:02 -0600

    explained stamp coupling

--- Bard.java
+++ Bard.java
@@ -15,7 +15,16 @@ public class Bard extends Character {
     protected void applyClassBonus() {
         coreAttributes.replace(Core.VIT, coreAttributes.get(Core.VIT) - 1);
     }
-
+    /**
+     * Example of Stamp coupling.
+     * The coupling exists because:
+     * The entire Weapon object is passed to the method as a parameter
+     * However, the method only uses two aspects of the weapon:
+     * It checks if the weapon exists in the character's inventory (weapons.contains(weapon))
+     * It calls the damage() method on the weapon
+     * This is stamp coupling because the method receives the entire Weapon complex data structure, 
+     * even though it only needs a small portion of its functionality.
+     */
     @Override
     protected int attack(Weapon weapon) {
         if(weapons.contains(weapon)){
